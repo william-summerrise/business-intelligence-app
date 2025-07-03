c840f2e20b05537a759871c59cc4ed26e6ab6737
William Ross <219585756+william-summerrise@users.noreply.github.com>
2025-07-03 12:55:14 -0400

    log service usage form

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home_page():
     return render_template('index.html')
+
+@app.route('/log-usage', methods=['GET', 'POST'])
+def log_usage():
+    feedback = None
+    if request.method == 'POST':
+        print('Got service data:')
+        print(f'  Name: {request.form["name"]}')
+        print(f'  Service: {request.form["service"]}')
+        print(f'  Sector: {request.form["sector"]}')
+        print(f'  Date: {request.form["date"]}')
+        feedback = 'Service Data Logged!'
+
+    return render_template('log-usage.html', feedback=feedback)
