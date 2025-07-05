62795f7dc3507947f00d1c71459802d44d1a6b0d
William Ross <219585756+william-summerrise@users.noreply.github.com>
2025-07-05 13:11:56 -0400

    validate form input exists

--- app.py
+++ app.py
@@ -12,15 +12,22 @@ def home_page():
 def log_usage():
     feedback = None
     if request.method == 'POST':
-        os.makedirs('log', exist_ok=True)
-        with open('log/service-data.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([
-                request.form['name'],
-                request.form['service'],
-                request.form['sector'],
-                request.form['date']
-            ])
-        feedback = 'Service Data Logged!'
+        # make sure all form inputs exist
+        if not request.form['name'] or \
+           not request.form['service'] or \
+           not request.form['sector'] or \
+           not request.form['date']:
+            feedback = 'There was an error logging form data, make sure you filled in all fields correctly!'
+        else:
+            os.makedirs('log', exist_ok=True)
+            with open('log/service-data.csv', 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([
+                    request.form['name'],
+                    request.form['service'],
+                    request.form['sector'],
+                    request.form['date']
+                ])
+            feedback = 'Service Data Logged!'
 
     return render_template('log-usage.html', feedback=feedback)
